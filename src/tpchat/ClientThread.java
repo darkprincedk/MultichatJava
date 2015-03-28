dd3ed3ebce058e9671a8625f56c66eb5f7357182
VictorTASSY <ram.31@hotmail.fr>
2015-03-28 18:56:46 +0100

    up server

--- src/tpchat/ClientThread.java
+++ src/tpchat/ClientThread.java
@@ -5,7 +5,14 @@
  */
 package tpchat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.System.exit;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,12 +22,62 @@ public class ClientThread extends Thread implements Runnable {
 
     private Socket socket;
     private ServerChat server;
+    private String instreamclient;
+    private HashMap<Socket, String> nick;
+    private boolean running;
 
-    
-    public ClientThread (Socket sock, ServerChat serv) {
-    this.socket = sock;
-    this.server = serv;
-   
-}
-    
+    public ClientThread(Socket sock, ServerChat serv) {
+        this.socket = sock;
+        this.server = serv;
+        this.nick = new HashMap<Socket, String>();
+        this.instreamclient = "";
+        this.running=true;
+        
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Client running ... " + socket);
+        server.broadcast(socket, instreamclient);
+        while (running && instreamclient!=null) {
+            instreamclient = getStream(socket);
+            if (instreamclient != null) {
+                if (instreamclient.startsWith("/nick")) {
+                    nick.put(socket, instreamclient.split(" ")[1]);
+                }
+                else if (instreamclient.startsWith("/close")){
+                    try {
+                        socket.close();
+                        running=false;
+                    } catch (IOException ex) {
+                        Logger.getLogger(ClientThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                
+                }
+                else if (nick.get(socket) != null) {
+                    server.broadcast(socket, "<" + nick.get(socket) + "> " + instreamclient);
+                } else {
+                    server.broadcast(socket, "<" + "???" + "> " + instreamclient);
+                }
+            }
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+      
+
+    }
+
+    private String getStream(Socket c) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+            return in.readLine();
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
 }
--- src/tpchat/ServerChat.java
+++ src/tpchat/ServerChat.java
@@ -14,31 +14,41 @@ import java.util.*;
  * @author kevin
  */
 public class ServerChat {
+
     private InetAddress iaddress;
     private int port;
-    HashMap <Socket,OutputStream> mess;
-    
-    public ServerChat(InetAddress address, int p) {
-this.iaddress = address;
-this.port = p;
-this.mess = new HashMap<>();
+    HashMap<Socket, OutputStream> map;
 
-}
-    public void start() throws IOException{
-    ServerSocket server = new ServerSocket(port,10,iaddress);
-    System.out.println("Server running ... "+port+" @ "+iaddress.getHostAddress());
-while (true) {
-Socket client = server.accept();
-client.getOutputStream().write("Welcome :D \n".getBytes());
-mess.put(client, client.getOutputStream());
-//ClientHandler ch = new ClientHandler(client, this);
-//ch.start();
-}
-    
+    public ServerChat(InetAddress address, int p) {
+        this.iaddress = address;
+        this.port = p;
+        this.map = new HashMap<>();
+
     }
-    
-    
-    
-    
-    
+
+    public void start() throws IOException {
+        ServerSocket server = new ServerSocket(port, 10, iaddress);
+        System.out.println("Server running ... " + port + " @ " + iaddress.getHostAddress());
+        while (true) {
+            Socket client = server.accept();
+            client.getOutputStream().write("!!!!Server Chat!!!! \n!!!OPTION!!!\n/nick <yournickname> to set your nickname\n/close to close the stream\n".getBytes());
+            map.put(client, client.getOutputStream());
+            ClientThread clThread = new ClientThread(client, this);
+            clThread.start();
+        }
+
+    }
+
+    public synchronized void broadcast(Socket s, String msg) throws IOException {
+        for (Map.Entry<Socket, OutputStream> entry : map.entrySet()) {
+            if (!entry.getKey().equals(s)) {
+                
+                   
+                        entry.getValue().write((msg + "\n").getBytes());
+                    
+               
+            }
+        }
+    }
+
 }
