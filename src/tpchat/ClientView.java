b664b57d67f68de34b3f81df28fd058208c079d3
VictorTASSY <ram.31@hotmail.fr>
2015-03-29 21:35:10 +0200

    begin view

---
+++ src/tpchat/ClientView.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpchat;
+
+import java.net.InetAddress;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kevin
+ */
+public class ClientView extends Application {
+
+    private InetAddress iaddress;
+    private int port;
+    
+    
+    ClientView(InetAddress address,int p) {
+        this.iaddress=address;
+        this.port=p;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
--- src/tpchat/Tpchat.java
+++ src/tpchat/Tpchat.java
@@ -24,36 +24,24 @@ import gnu.getopt.LongOpt;
  * @author kevin
  */
 public class Tpchat extends Application {
-
+    private static InetAddress add;
+       private static int port = 5000;
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage primaryStage) throws Exception {
+        ClientView cv ;
+        cv = new ClientView(add,port);
+        cv.start(primaryStage);
+}
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
 
     /**
      * @param args the command line arguments
      * @throws java.net.UnknownHostException
      */
     public static void main(String[] args) throws UnknownHostException, IOException {
-        InetAddress add = InetAddress.getByName("127.0.0.1");
-        int port = 5000;
+        add = InetAddress.getByName("127.0.0.1");
         String help = "Utilisation : java -jar target/multichat -0.0.1-SNAPSHOT.jar [OPTION]...\n"
                 + "-a, --address=ADDR spécifier l'adresse IP\n"
                 + "-d, --debug affiche les messages d'erreur\n"
@@ -75,7 +63,7 @@ public class Tpchat extends Application {
         longopts[2] = new LongOpt("help", LongOpt.NO_ARGUMENT, null, 'h');
         longopts[3] = new LongOpt("multicast", LongOpt.NO_ARGUMENT, null, 'm');
         longopts[4] = new LongOpt("nio", LongOpt.NO_ARGUMENT, null, 'n');
-        longopts[5] = new LongOpt("port", LongOpt.NO_ARGUMENT, null, 'p');
+        longopts[5] = new LongOpt("port", LongOpt.REQUIRED_ARGUMENT, null, 'p');
         longopts[6] = new LongOpt("server", LongOpt.NO_ARGUMENT, null, 's');
         Getopt g = new Getopt("Multichat", args, "a:dhmnp:s", longopts);
         int c;
@@ -133,7 +121,10 @@ public class Tpchat extends Application {
         
         }
         else{
-        System.out.println("interface client pas encore implémenté\n");
+                
+                launch(args);
+               
+                
         
         }
             
@@ -149,4 +140,6 @@ public class Tpchat extends Application {
          System.out.println("coucou");
          snio.start();*/
     }
+
+    
 }
